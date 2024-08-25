415f35fe0f6d47939fa51e705fd176402a74b828
Hyun-je <bigae2@gmail.com>
2024-08-25 15:42:57 +0900

    Refactor EarlyStopping to increase patience to 7

--- solver.py
+++ solver.py
@@ -140,7 +140,7 @@ class Solver(object):
         path = self.model_save_path
         if not os.path.exists(path):
             os.makedirs(path)
-        early_stopping = EarlyStopping(patience=3, verbose=True, dataset_name=self.dataset)
+        early_stopping = EarlyStopping(patience=7, verbose=True, dataset_name=self.dataset)
         train_steps = len(self.train_loader)
 
         for epoch in range(self.num_epochs):
